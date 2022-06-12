80914b8918a1a0d79bc91967a8183fe99da0169e
OnahyraG <103009057+OnahyraG@users.noreply.github.com>
2022-06-12 19:45:41 -0300

    complementario de arrays

--- adicionales.js
+++ adicionales.js
@@ -9,14 +9,15 @@ class servicio {
 
 }
 
-const servicios = [
+const servicios = [];  
 
-    new servicio(1, ' creacion y mantenimiento de pagina wed', 6.99),
-    new servicio(2, ' creacion y mantenimiento de redes sociales', 7.99),
-    new servicio(3, ' diseño grafico y marketing disital', 6.99),
-    new servicio(4, ' seo y posicionamiento en motores de busqueda', 6.99),
 
-]
+    let servicio1 = new servicio (1, ' creacion y mantenimiento de pagina wed', 6.99);
+    let servicio2 = new servicio(2, ' creacion y mantenimiento de redes sociales', 7.99);
+    let servicio3 = new servicio(3, ' diseño grafico y marketing disital', 6.99);
+    let servicio4 = new servicio(4, ' seo y posicionamiento en motores de busqueda', 6.99);
+
+servicios.push(servicio1, servicio2, servicio3, servicio4);
+  
 
 console.log(servicios);
-
